4ba46be22b466968088073e6e62b809377c4b2be
sayehjarollahi <sjarollahi@yahoo.com>
2023-11-30 23:23:14 +0330

    add calculator

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+import java.lang.Math;
+
+public class Calculator {
+    public double sqrtDivide(int a, int b) {
+        double tmp = (double) a / (double) b;
+        return Math.sqrt(tmp);
+    }
+}
